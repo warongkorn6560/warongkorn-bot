ad035ad68c4c44418097d9bdea6dba5017a53d39
🥓 Most-5Lab <most_5lab@192.168.0.101>
2021-08-23 23:06:41 +0700

    update

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
-  reply(reply_token)
+  let msg = req.body.events[0].message.text
+  reply(reply_token, msg)
   res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+function reply(reply_token, msg) {
   let headers = {
     'Content-Type': 'application/json',
     Authorization:
@@ -22,11 +23,7 @@ function reply(reply_token) {
     messages: [
       {
         type: 'text',
-        text: 'Hello',
-      },
-      {
-        type: 'text',
-        text: 'How are you?',
+        text: msg,
       },
     ],
   })
