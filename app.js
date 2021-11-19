ccf741a06c491b16157adf7c1035d0b4baccf586
🥓 Most-5Lab <most_5lab@Warongkorns-MacBook-Pro.local>
2021-11-19 20:57:33 +0700

    aiml

--- app.js
+++ app.js
@@ -1,83 +1,39 @@
 // // Reply using AIML
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// const AIMLInterpreter = require('aimlinterpreter')
-
-// const app = express()
-// const port = process.env.PORT || 4000
-// const aimlInterpreter = new AIMLInterpreter({ name: 'WarongkornBot' })
-
-// aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-
-// app.post('/webhook', (req, res) => {
-//   let reply_token = req.body.events[0].replyToken
-//   let msg = req.body.events[0].message.text
-//   aimlInterpreter.findAnswerInLoadedAIMLFiles(
-//     msg,
-//     (answer, wildCardArray, input) => {
-//       reply(reply_token, answer)
-//     }
-//   )
-//   res.sendStatus(200)
-// })
-
-// app.listen(port)
-
-// function reply(reply_token, msg) {
-//   let headers = {
-//     'Content-Type': 'application/json',
-//     Authorization: 'Bearer {xxxxx}',
-//   }
-
-//   let body = JSON.stringify({
-//     replyToken: reply_token,
-//     messages: [
-//       {
-//         type: 'text',
-//         text: msg,
-//       },
-//     ],
-//   })
-
-//   request.post(
-//     {
-//       url: 'https://api.line.me/v2/bot/message/reply',
-//       headers: headers,
-//       body: body,
-//     },
-//     (err, res, body) => {
-//       console.log('status = ' + res.statusCode)
-//     }
-//   )
-// }
-
-// d74fa631e540526a6e2152b614299049
-// Echo reply
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const AIMLInterpreter = require('aimlinterpreter')
+
 const app = express()
 const port = process.env.PORT || 4000
+const aimlInterpreter = new AIMLInterpreter({ name: 'WarongkornBot' })
+
+aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
   let msg = req.body.events[0].message.text
-  reply(reply_token, msg)
+  aimlInterpreter.findAnswerInLoadedAIMLFiles(
+    msg,
+    (answer, wildCardArray, input) => {
+      reply(reply_token, answer)
+    }
+  )
   res.sendStatus(200)
 })
+
 app.listen(port)
+
 function reply(reply_token, msg) {
   let headers = {
     'Content-Type': 'application/json',
     Authorization:
       'Bearer duMhSiSLxyiZ/ROTJXtEfZOZ1En2WyGbmgXnz6OkbWcvCLmcDRFi9tpzHsaSa7Gi6hRn6cFJlqMBRdYLsTDRQ54Qk3H8uwGEKlo53Ha48kE+7xDHrap+BVPPBseg2cMT5HWFlfl+r1uz557jc6EY5QdB04t89/1O/w1cDnyilFU=',
   }
+
   let body = JSON.stringify({
     replyToken: reply_token,
     messages: [
@@ -87,6 +43,7 @@ function reply(reply_token, msg) {
       },
     ],
   })
+
   request.post(
     {
       url: 'https://api.line.me/v2/bot/message/reply',
@@ -98,3 +55,47 @@ function reply(reply_token, msg) {
     }
   )
 }
+
+// d74fa631e540526a6e2152b614299049
+// Echo reply
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const request = require('request')
+// const app = express()
+// const port = process.env.PORT || 4000
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.post('/webhook', (req, res) => {
+//   let reply_token = req.body.events[0].replyToken
+//   let msg = req.body.events[0].message.text
+//   reply(reply_token, msg)
+//   res.sendStatus(200)
+// })
+// app.listen(port)
+// function reply(reply_token, msg) {
+//   let headers = {
+//     'Content-Type': 'application/json',
+//     Authorization:
+//       'Bearer duMhSiSLxyiZ/ROTJXtEfZOZ1En2WyGbmgXnz6OkbWcvCLmcDRFi9tpzHsaSa7Gi6hRn6cFJlqMBRdYLsTDRQ54Qk3H8uwGEKlo53Ha48kE+7xDHrap+BVPPBseg2cMT5HWFlfl+r1uz557jc6EY5QdB04t89/1O/w1cDnyilFU=',
+//   }
+//   let body = JSON.stringify({
+//     replyToken: reply_token,
+//     messages: [
+//       {
+//         type: 'text',
+//         text: msg,
+//       },
+//     ],
+//   })
+//   request.post(
+//     {
+//       url: 'https://api.line.me/v2/bot/message/reply',
+//       headers: headers,
+//       body: body,
+//     },
+//     (err, res, body) => {
+//       console.log('status = ' + res.statusCode)
+//     }
+//   )
+// }
