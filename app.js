d6e926df22aa00930aa35fe1f86d707476aefb3d
🥓 Most-5Lab <most_5lab@Warongkorns-MacBook-Pro.local>
2021-11-19 19:08:40 +0700

    update

--- app.js
+++ app.js
@@ -1,48 +1,91 @@
-// Reply using AIML
+// // Reply using AIML
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const request = require('request')
+// const AIMLInterpreter = require('aimlinterpreter')
+
+// const app = express()
+// const port = process.env.PORT || 4000
+// const aimlInterpreter = new AIMLInterpreter({ name: 'WarongkornBot' })
+
+// aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+
+// app.post('/webhook', (req, res) => {
+//   let reply_token = req.body.events[0].replyToken
+//   let msg = req.body.events[0].message.text
+//   aimlInterpreter.findAnswerInLoadedAIMLFiles(
+//     msg,
+//     (answer, wildCardArray, input) => {
+//       reply(reply_token, answer)
+//     }
+//   )
+//   res.sendStatus(200)
+// })
+
+// app.listen(port)
+
+// function reply(reply_token, msg) {
+//   let headers = {
+//     'Content-Type': 'application/json',
+//     Authorization: 'Bearer {xxxxx}',
+//   }
+
+//   let body = JSON.stringify({
+//     replyToken: reply_token,
+//     messages: [
+//       {
+//         type: 'text',
+//         text: msg,
+//       },
+//     ],
+//   })
+
+//   request.post(
+//     {
+//       url: 'https://api.line.me/v2/bot/message/reply',
+//       headers: headers,
+//       body: body,
+//     },
+//     (err, res, body) => {
+//       console.log('status = ' + res.statusCode)
+//     }
+//   )
+// }
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLInterpreter = require('aimlinterpreter')
-
 const app = express()
 const port = process.env.PORT || 4000
-const aimlInterpreter = new AIMLInterpreter({ name: 'WarongkornBot' })
-
-aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
-  let msg = req.body.events[0].message.text
-  aimlInterpreter.findAnswerInLoadedAIMLFiles(
-    msg,
-    (answer, wildCardArray, input) => {
-      reply(reply_token, answer)
-    }
-  )
+  reply(reply_token)
   res.sendStatus(200)
 })
-
 app.listen(port)
-
-function reply(reply_token, msg) {
+function reply(reply_token) {
   let headers = {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer {xxxxx}',
+    Authorization: 'Bearer {xxxxxxx}',
   }
-
   let body = JSON.stringify({
     replyToken: reply_token,
     messages: [
       {
         type: 'text',
-        text: msg,
+        text: 'Hello',
+      },
+      {
+        type: 'text',
+        text: 'How are you?',
       },
     ],
   })
-
   request.post(
     {
       url: 'https://api.line.me/v2/bot/message/reply',
