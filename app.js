b8b1b359bc03715da17dbded29d849ffd8055ab3
🥓 Most-5Lab <most_5lab@Warongkorns-MacBook-Pro.local>
2021-11-20 15:42:28 +0700

    aiml

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
   let msg = req.body.events[0].message.text
   aimlParser.getResult(msg, (answer, wildCardArray, input) => {
-    reply(reply_token, input)
+    reply(reply_token, answer)
   })
   res.sendStatus(200)
 })
